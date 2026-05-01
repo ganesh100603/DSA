0a32dd6375aea47e74c675a374995df1a4b64b1b
Ganesh Drona <137412373+ganesh100603@users.noreply.github.com>
2026-05-02 01:02:22 +0530

    Time: 1 ms (99.58%) | Memory: 47.2 MB (75.03%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length == 1) return 0;
+        int far = 0;
+        int currentRange = 0;
+        int jumps = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            far = Math.max(far,i+nums[i]);
+            if(far >= nums.length - 1){
+                jumps++;
+                break;
+            }
+            if(i==currentRange){
+                jumps++;
+                currentRange = far;
+            }
+        }
+        return jumps;
+    }
+}
