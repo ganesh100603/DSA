bc17e2244a36c2817f330e5aa683548efa884038
Ganesh Drona <137412373+ganesh100603@users.noreply.github.com>
2026-07-15 23:43:15 +0530

    Time: 27 ms (92.55%) | Memory: 47.7 MB (89.99%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,20 +1,15 @@
 class Solution {
     public String removeStars(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        
-        for(char c : s.toCharArray()){
-            if(c == '*'){
-                stack.pop();
-            }else{
-                stack.push(c);
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
             }
         }
 
-        StringBuilder str = new StringBuilder();
-
-        while(!stack.isEmpty()){
-            str.append(stack.removeLast());
-        }
-        return str.toString();
+        return sb.toString();
     }
 }
